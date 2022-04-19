bfd25fe61a97d5c12f8326cf88f1bf8561e41ecf
Ken Hsu <knel.tw@gmail.com>
2022-04-19 23:31:21 +0800

    feat(deploy): deploy to Rinkeby
    * [Notebook Contract](https://rinkeby.etherscan.io/address/0xDB6455D1053AFA5F33dFb7607e239246FeBFf508)
    
    * [Proxy Contract](https://rinkeby.etherscan.io/address/0xb87496f41Dfd754c690f2e068d202686E5285ED4)

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,10 +2,14 @@ import { ethers } from "hardhat";
 
 const deploy = async () => {
   const Notebook = await ethers.getContractFactory("Notebook");
-  const notebook = await Notebook.deploy("Notebook", "NG");
+  const notebook = await Notebook.deploy("Notebook", "NBK");
   console.log("Contract deployed to address:", notebook.address);
   const result = await notebook.deployTransaction.wait();
   console.log("gasUsed:", result.gasUsed);
+  const Proxy = await ethers.getContractFactory("Proxy");
+  const proxy = await Proxy.deploy(notebook.address);
+  console.log("Contract deployed to address:", proxy.address);
+  const r = await proxy.deployTransaction.wait();
 };
 deploy()
   // eslint-disable-next-line no-process-exit
